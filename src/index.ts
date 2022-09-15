e01edcb506e3f595cb33313e0cce5aca03187351
filibit <filip.biterski@gmail.com>
2022-09-15 02:21:31 +0200

    fix: remove deleted methods from export

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
-import {
-  isArray,
-  isArrayOf,
-  isNonEmptyArray,
-  mustBeArray,
-  mustBeArrayOf,
-  mustBeNonEmptyArray,
-} from './arrays'
+import { isArray, isNonEmptyArray, mustBeArray, mustBeNonEmptyArray } from './arrays'
 import { isFunction, mustBeFunction } from './functions'
 import { isPlainObject, mustBePlainObject } from './plainObjects'
 import { isNumber, isString, mustBeNumber, mustBeString } from './primitives'
 
 export {
   isArray,
-  isArrayOf,
   isNonEmptyArray,
   mustBeArray,
-  mustBeArrayOf,
   mustBeNonEmptyArray,
   isFunction,
   mustBeFunction,
