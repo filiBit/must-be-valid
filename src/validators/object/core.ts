f21962800e54001a4da01c538e2e6137bd971b43
filibit <filip.biterski@gmail.com>
2022-10-03 02:14:10 +0200

    fix: update `mustBeObject` type annotation
    - improve type inferencing

--- src/validators/object/core.ts
+++ src/validators/object/core.ts
@@ -11,11 +11,10 @@ export function isObject<Key extends string | number | symbol, Value = unknown>(
  * Returns the `value` if it is an `object` and also isn't an array or `null`.
  * @throws Will throw an error otherwise.
  */
-export function mustBeObject(
-  value: unknown
-): typeof value extends object
-  ? typeof value
-  : { [key: string | number | symbol]: unknown } {
-  if (isObject(value)) return value
+export function mustBeObject<T>(
+  value: T
+): T extends object ? T : { [key: string | number | symbol]: unknown } {
+  if (isObject(value))
+    return value as T extends object ? T : { [key: string | number | symbol]: unknown }
   throw new Error('The value must be an object')
 }
--- types.d.ts
+++ types.d.ts
@@ -326,9 +326,7 @@ declare module 'must-be-valid' {
    * Returns the `value` if it is an `object` and also isn't an array or `null`.
    * @throws Will throw an error otherwise.
    */
-  export function mustBeObject(value: unknown): typeof value extends object
-    ? typeof value
-    : {
-        [key: string | number | symbol]: unknown
-      }
+  export function mustBeObject<T>(
+    value: T
+  ): T extends object ? T : { [key: string | number | symbol]: unknown }
 }
